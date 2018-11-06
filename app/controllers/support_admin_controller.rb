2f7420d7a28d7d861d7e19529d40caf18881c9e2
Chaïb Martinez <chaibax@gmail.com>
2018-11-06 11:40:37 +0100

    Spellchecking

--- app/controllers/support_admin_controller.rb
+++ app/controllers/support_admin_controller.rb
@@ -16,7 +16,7 @@ class SupportAdminController < ApplicationController
       redirect_to root_path
     else
       setup_context
-      flash.now.alert = "Une erreur est survenue. Vous pouvez nous contactez à #{helpers.mail_to(CONTACT_EMAIL)}."
+      flash.now.alert = "Une erreur est survenue. Vous pouvez nous contacter à #{helpers.mail_to(CONTACT_EMAIL)}."
 
       render :index
     end
